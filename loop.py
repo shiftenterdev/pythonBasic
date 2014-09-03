e0b99931535e264c6d0f7f9282577908eb34c9e2
Bappa <iftakharul.alam@live.com>
2014-09-03 13:00:32 -0800

    collection for loop

--- loop.py
+++ loop.py
@@ -1,7 +1,14 @@
-#while loop
+#for loop
 def apple():
 
     y = 1
     for y in range(5,10):
         print(y)
 apple()
+
+#for loop for collection
+def simple():
+    names = ["bappa","abir","sumon"]
+    for n in names:
+        print(n);
+simple()
