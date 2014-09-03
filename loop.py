a94ab7767dd765d3d2c48b84ee86d66396a29c86
Bappa <iftakharul.alam@live.com>
2014-09-03 13:18:39 -0800

    continue statement

--- loop.py
+++ loop.py
@@ -6,17 +6,34 @@ def apple():
         print(y)
 apple()
 
+
 #for loop for collection
+
+
 def simple():
     names = ["bappa","abir","sumon"]
     for n in names:
         print(n)
 simple()
 
+
 #break statement
+
+
 def demo():
     for x in range(10,20):
         if(x==15):
              break
         print(x)
 demo()
+
+
+#continue statement
+
+
+def demo1():
+    for x in range(11,20):
+        if(x%2 == 0):
+            continue
+        print(x)
+demo1()
