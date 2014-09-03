234f452158021bfb7588bc00eafb93211c412029
Bappa <iftakharul.alam@live.com>
2014-09-03 12:40:46 -0800

    python condition

---
+++ condition.py
@@ -0,0 +1,11 @@
+def main():
+    x, y = 1000, 100
+    if(x < y):
+        st = "x is less than y"
+    elif(x == y):
+        st = "x and y are same"
+    else:
+        st = "x is greater than y"
+    print(st)
+
+main()
--- function.py
+++ function.py
@@ -2,6 +2,7 @@
 #each statement indented by 2 space corresponding
 # that function and also a column after the parenthesis
 
+
 def bappa():
     print("This is python function")
 #bappa()
@@ -10,30 +11,34 @@ print(bappa())  # print statement and return the value -> none
 
 print(bappa)  # print as an object
 
-def bappa(arg1,arg2):
-    print(arg1," ",arg2)
+
+def bappa(arg1, arg2):
+    print(arg1, " ", arg2)
 
 bappa(20,30)
 print(bappa(20,30))
+
 
 def cube(x):
     return x*x*x
 print(cube(20))
 
-def power(num,x=1):
+
+def power(num, x=1):
     result = 1
     for i in range(x):
         result = result * num
     return result
 
 print(power(3))
-print(power(3,4))
-print(power(x=4,num=3))
+print(power(3, 4))
+print(power(x=4, num=3))
+
 
 def mult_add(*args):
     result = 0
     for i in args:
         result += i
     return result
-print(mult_add(10,20,40))
-print(mult_add(100,200))
+print(mult_add(10, 20, 40))
+print(mult_add(100, 200))
