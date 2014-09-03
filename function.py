15a40ed42e712f48c1a2a854cf2ba376e00b1fbe
Bappa <iftakharul.alam@live.com>
2014-09-03 11:15:34 -0800

    python function with for loop

--- function.py
+++ function.py
@@ -19,3 +19,12 @@ print(bappa(20,30))
 def cube(x):
     return x*x*x
 print(cube(20))
+
+def power(num,x=1):
+    result = 1
+    for i in range(x):
+        result = result * num
+    return result
+
+print(power(3))
+print(power(3,4))
