73c6386eac7fb83916d8cbd69fedbf5e2ecea78d
Bappa <iftakharul.alam@live.com>
2014-09-03 11:06:04 -0800

    python function return and object type

--- function.py
+++ function.py
@@ -1,7 +1,11 @@
 #define a function by def keyword
 #each statement indented by 2 space corresponding
-# that function
+# that function and also a column after the parenthesis
 
 def bappa():
     print("This is python function")
-bappa()
+#bappa()
+
+print(bappa())  # print statement and return the value -> none
+
+print(bappa)  # print as an object
