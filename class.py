7bc78016d9d0a4a6885a03e5f2071fc008e81e06
Bappa <iftakharul.alam@live.com>
2014-09-05 09:15:54 -0800

    intro to class

--- class.py
+++ class.py
@@ -1,8 +1,10 @@
 class sample():
     def demo1(self):
         print("This a base method")
-    def demo2(self,string):
+
+    def demo2(self, string):
         print("This is extended method "+string)
+
 
 def main():
     bappa = sample()
